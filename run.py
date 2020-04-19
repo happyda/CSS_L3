cfcadf26194ab635249c8c7a1456621ba36d079a
SodiumFoo <ul2you@gmail.com>
2020-04-19 12:35:58 +0000

    增加About/近期文章頁面,己到最後。

--- run.py
+++ run.py
@@ -3,8 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template('index.html')
+
+@app.route("/recent-post")
+def recentpost():
+    return render_template('recent-post.html')
+
+@app.route("/about-me")
+def about_me():
+    return render_template('about-me.html')
 
 if __name__ == '__main__':
 
